39539983141c63d440c8dc644b354191e04fb92b
Roux <roux@smoo.ai>
2025-03-06 17:41:12 -0500

    Update package 2025-03-06 17:41:12 olive shark

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
-    entry: ['src/sleep.ts','src/phoneNumber.ts','src/phoneNumber.spec.ts','src/hono.ts','src/hono.spec.ts','src/findFile.ts','src/findFile.spec.ts','src/errorHandler.ts','src/env.ts','src/apiHandler.ts','src/CaseInsensitiveSet.ts','src/CaseInsensitiveSet.spec.ts','src/CaseInsensitiveMap.ts','src/CaseInsensitiveMap.spec.ts','src/ApiError.ts','src/scripts/createEntryPoints.ts'],
+    entry: ['src/sleep.ts','src/phoneNumber.ts','src/hono.ts','src/findFile.ts','src/errorHandler.ts','src/env.ts','src/apiHandler.ts','src/CaseInsensitiveSet.ts','src/CaseInsensitiveMap.ts','src/ApiError.ts','src/scripts/createEntryPoints.ts'],
     clean: true,
     dts: false, // Turned off due to issues with @supabase/supabase-js/lib/types because of tyepof fetch and an error about it being private.
     format: ['cjs'],
