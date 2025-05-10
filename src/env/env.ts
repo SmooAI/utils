c581cb3654d8500990c19fb6ac8092d2a81b5e58
Brent Rager <brent@rager.tech>
2025-05-10 12:18:50 -0400

    Added isRunningInBrowser. (#72)

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,7 +1,13 @@
+/// <reference lib="dom" />
+
 export function isRunningLocally(): boolean {
     return !!process.env.SST_DEV || !!process.env.IS_LOCAL || process.env.IS_DEPLOYED_STAGE !== 'true';
 }
 
 export function isRunningInProd(): boolean {
     return process.env.SST_STAGE === 'prod' || process.env.SST_STAGE === 'production' || process.env.IS_DEPLOYED_STAGE === 'true';
+}
+
+export function isRunningInBrowser(): boolean {
+    return typeof window !== 'undefined' && typeof window.document !== 'undefined';
 }
